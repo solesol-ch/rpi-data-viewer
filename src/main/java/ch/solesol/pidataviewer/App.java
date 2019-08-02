c2b9d38a879372fa6497b32618de5dfefeb805d4
Yannis Huber <yannis.h@hotmail.ch>
2019-08-02 15:23:47 +0200

    Performance test

--- src/main/java/ch/solesol/pidataviewer/App.java
+++ src/main/java/ch/solesol/pidataviewer/App.java
@@ -4,19 +4,28 @@
 package ch.solesol.pidataviewer;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 320, 240);
+        Label l = new Label();
+        SimpleStringProperty s = new SimpleStringProperty("Clicks: 0");
+        l.textProperty().bind(s);
+        Button b = new Button("Touch me");
+        AtomicInteger clicks = new AtomicInteger();
+        b.setOnAction((e) -> {
+            s.setValue(String.format("Clicks : %d", clicks.incrementAndGet()));
+        });
+        Scene scene = new Scene(new StackPane(b, l), 320, 240);
         stage.setScene(scene);
         stage.show();
     }
